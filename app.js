5bfd4fbbbf169322c03a2e6a95b064b29fca5af7
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-10-26 17:40:58 +0900

    음식점 등록 -> 게시판 확인

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const express = require('express')
 // app을 express 기반 객체로 만듬. -> express 내에 있는 함수들을 사용이 가능해짐.
 const app = express()
 
+// express에 특정 옵션을 줄 수 있음.
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
 // 참조되는 이미지, 파일 등의 기본 경로를 지정해주는 역할
 // 정적인 부분 : .use(express.static(기본 경로가 될 파일명))
 app.use(express.static('public'))
@@ -19,14 +23,16 @@ app.use(express.urlencoded({ extended: false }))
 
 // 경로를 만들어줘야함.
 app.get('/', function (req, res) {
-  const htmlFielPath = path.join(__dirname, 'views', 'index.html')
   // path를 불러와서 path.join을 통해 경로를 할당해주고 그 경로를 통해 해당 파일을 응답으로 보내준다.
   // 보내주는 것 : sendFile()
-  res.sendFile(htmlFielPath)
+  // const htmlFielPath = path.join(__dirname, 'views', 'index.html')
+  // res.sendFile(htmlFielPath)
+
+  // ejs로 엔진을 바꾸고 나서 render() 메소드 사용
+  res.render('index')
 })
 app.get('/about', function (req, res) {
-  const htmlFielPath = path.join(__dirname, 'views', 'about.html')
-  res.sendFile(htmlFielPath)
+  res.render('about')
 })
 // 입력처리를 하기 위해서 .post()!!
 app.post('/recommend', function (req, res) {
@@ -35,7 +41,6 @@ app.post('/recommend', function (req, res) {
   const filePath = path.join(__dirname, 'data', 'restaurants.json')
 
   const fileData = fs.readFileSync(filePath)
-  console.log(fileData)
   const storedRestaurants = JSON.parse(fileData)
 
   storedRestaurants.push(restaurant)
@@ -45,16 +50,21 @@ app.post('/recommend', function (req, res) {
 })
 
 app.get('/confirm', function (req, res) {
-  const htmlFielPath = path.join(__dirname, 'views', 'confirm.html')
-  res.sendFile(htmlFielPath)
+  res.render('confirm')
 })
 app.get('/recommend', function (req, res) {
-  const htmlFielPath = path.join(__dirname, 'views', 'recommend.html')
-  res.sendFile(htmlFielPath)
+  res.render('recommend')
 })
 app.get('/restaurants', function (req, res) {
-  const htmlFielPath = path.join(__dirname, 'views', 'restaurants.html')
-  res.sendFile(htmlFielPath)
+  const filePath = path.join(__dirname, 'data', 'restaurants.json')
+
+  const fileData = fs.readFileSync(filePath)
+  const storedRestaurants = JSON.parse(fileData)
+  // ejs 문법으로 동적인 값 전달하기
+  res.render('restaurants', {
+    restaurantsNum: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  })
 })
 
 // 특정포트에서 들어오는 네트워크 트래픽을 감지하게 됨.
