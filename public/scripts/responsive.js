843eeb118a82e210daca4b2609ed8e3abcb1096d
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-10-27 15:19:21 +0900

    ejs 문법 2일차

--- public/scripts/responsive.js
+++ public/scripts/responsive.js
@@ -2,6 +2,7 @@ const drawerBtnElement = document.getElementById('drawer-btn')
 const mobileDarwerElement = document.getElementById('mobile-drawer')
 
 function toggleDrawer() {
+  console.log('in')
   mobileDarwerElement.classList.toggle('open')
 }
 
